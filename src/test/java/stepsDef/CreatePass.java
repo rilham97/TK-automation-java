d15c0356bb56b8f1a6f6afd062afe32f2c006074
Ilham Ramadhan <ilham.ramadhan@mitrais.com>
2020-06-26 12:24:46 +0800

    Rename file stepsDef

--- src/test/java/stepsDef/CreatePass.java
+++
@@ -1,5 +0,0 @@
-package stepsDef;
-
-public class CreatePass {
-
-}
--- src/test/java/stepsDef/LoginEmailStep.java
+++ src/test/java/stepsDef/LoginEmailStep.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class LoginEmailTest {
+public class LoginEmailStep {
 	LoginFunction login = new LoginFunction();
 	
 	@Given("User has accessed Login page")
