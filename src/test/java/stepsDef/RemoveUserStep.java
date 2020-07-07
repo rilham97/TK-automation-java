e6a40e6dec540ea5ce77735ad9b8aa6f192ffe9c
Ronny Dacosta <RonnyWoren.Dacosta@mitrais.com>
2020-07-07 14:12:32 +0800

    complete function and step for remove user

---
+++ src/test/java/stepsDef/RemoveUserStep.java
@@ -0,0 +1,56 @@
+package stepsDef;
+
+import base.LoginFunction;
+import base.RemoveUserFunction;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RemoveUserStep {
+    LoginFunction loginFunction = new LoginFunction();
+    RemoveUserFunction removeUserFunction = new RemoveUserFunction();
+
+    @Given("User in the home screen")
+    public void user_in_the_home_screen() throws InterruptedException {
+        loginFunction.verifyLoginPage();
+        loginFunction.inputEmail("remove@gmail.com");
+        loginFunction.inputPassword("R3move!");
+        loginFunction.pressLogin();
+        loginFunction.verifyHomePage();
+    }
+
+    @When("User click hamburger button")
+    public void user_click_hamburger_button() {
+       removeUserFunction.clickHamburgerButton();
+    }
+
+    @When("User click on my account menu")
+    public void user_click_on_my_account_menu() {
+        removeUserFunction.clickMyAccount();
+    }
+
+    @When("Click on delete account")
+    public void click_on_delete_account() {
+        removeUserFunction.clickDeleteAccount();
+    }
+
+    @When("Click on continue button from the popup confirmation")
+    public void click_on_continue_button_from_the_popup_confirmation() {
+        removeUserFunction.clickContinue();
+    }
+
+    @Then("User directed to home screens")
+    public void user_directed_to_home_screens() {
+        removeUserFunction.verifyHomeScreen();
+    }
+
+    @Then("User can't login with the account that already removed")
+    public void user_can_t_login_with_the_account_that_already_removed() throws InterruptedException {
+        loginFunction.verifyLoginPage();
+        loginFunction.inputEmail("remove@gmail.com");
+        loginFunction.inputPassword("R3move!");
+        loginFunction.pressLogin();
+        removeUserFunction.checkIncorrectEmailPassword();
+    }
+
+}
--- src/test/resources/features/RemoveUser.feature
+++ src/test/resources/features/RemoveUser.feature
@@ -5,7 +5,7 @@ Feature: Remove User
 
   Scenario: User want to remove account
     Given User in the home screen
-    When User click humburger button
+    When User click hamburger button
     And User click on my account menu
     And Click on delete account
     And Click on continue button from the popup confirmation
