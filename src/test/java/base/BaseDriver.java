92201f9e3162059c8b4faadc56a02bff6953b7ee
Ronny Dacosta <RonnyWoren.Dacosta@mitrais.com>
2020-07-09 13:39:33 +0800

    fix base driver

--- src/test/java/base/BaseDriver.java
+++ src/test/java/base/BaseDriver.java
@@ -4,6 +4,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.PropertiesReader;
 
 import java.net.MalformedURLException;
 import java.net.URL;
