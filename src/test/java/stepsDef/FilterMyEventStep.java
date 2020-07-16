015fca4398d51dc02ba2b0d15079190aea515aa6
Ronny Dacosta <RonnyWoren.Dacosta@mitrais.com>
2020-07-16 14:49:44 +0800

    complete feature, step & function for filter my current event

---
+++ src/test/java/stepsDef/FilterMyEventStep.java
@@ -0,0 +1,41 @@
+package stepsDef;
+
+import base.CancelApplicationFunction;
+import base.FilterMyEventFunction;
+import base.LoginFunction;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class FilterMyEventStep {
+    LoginFunction loginFunction = new LoginFunction();
+    CancelApplicationFunction cancelApplicationFunction = new CancelApplicationFunction();
+    FilterMyEventFunction filterMyEventFunction = new FilterMyEventFunction();
+
+    @Given("user already in the my applied event screen")
+    public void user_already_in_the_my_applied_event_screen() throws InterruptedException {
+        loginFunction.verifyLoginPage();
+        loginFunction.inputEmail("filter2@email.com");
+        loginFunction.inputPassword("Filter2!");
+        loginFunction.pressLogin();
+        loginFunction.verifyHomePage();
+
+        cancelApplicationFunction.clickHamburgerButton();
+        cancelApplicationFunction.clickAppliedEvents();
+    }
+
+    @When("user click on filter icon")
+    public void user_click_on_filter_icon() {
+        filterMyEventFunction.clickFilterIcon();
+    }
+
+    @When("click on one of {string} filter")
+    public void click_on_one_of_filter(String inputUser) {
+        filterMyEventFunction.clickFilterMethod(inputUser);
+    }
+
+    @Then("application will display only events with {string} status")
+    public void application_will_display_only_events_with_status(String inputUser) {
+        filterMyEventFunction.verifyEventBoxStatus(inputUser);
+    }
+}
--- src/test/resources/features/FilterInMyEvent.feature
+++ src/test/resources/features/FilterInMyEvent.feature
@@ -5,11 +5,12 @@ Feature: Filter in my event
 
   Scenario Outline: User want to filter my current event
     Given user already in the my applied event screen
-    When user click on icon filter
+    When user click on filter icon
     And click on one of <Method> filter
     Then application will display only events with <METHOD> status
 
     Examples:
-      | Method    | METHOD    |
-      | "Applied" | "APPLIED" |
-
+      | Method     | METHOD    |
+      | "Applied"  | "APPLIED" |
+      | "Accepted" | "ACCEPTED"|
+      | "Rejected" | "REJECTED"|
