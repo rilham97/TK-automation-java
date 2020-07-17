5b28206e16ad95867f15c2eb8cba4f9196d3d7d8
Ilham Ramadhan <ilham.ramadhan@mitrais.com>
2020-07-17 09:03:04 +0800

    Fix Setup DB and tearDown on every scenario

--- src/test/java/stepsDef/Hooks.java
+++ src/test/java/stepsDef/Hooks.java
@@ -1,17 +1,20 @@
 package stepsDef;
 import base.BaseDriver;
 import io.cucumber.java.*;
+import utilities.DBCall;
 public class Hooks {
 	BaseDriver driver = new BaseDriver();
 	@Before
 	public void beforeScenario() {
 		System.out.println("This will run before the Scenario");
 		driver.setUp();
+		DBCall.setUpDB();
 	}
 	
 	@After
 	public void afterScenario() {
 		System.out.println("This will run after the Scenario");
+		DBCall.tearDownDB();
 		driver.tearDown();
 	}
 }
